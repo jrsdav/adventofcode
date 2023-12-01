cc8d952b4b452097c14d6a23c01eeb80b7ecc456
Joey Davenport <joey.davenport@recursionpharma.com>
2023-12-01 10:47:25 -0700

    2023: Solution for day 1 part 1

---
+++ 2023/01/p1/solution.py
@@ -0,0 +1,21 @@
+input = open("input.txt", "r")
+
+def extract_callibration_value(input):
+  
+	results = []
+  	
+	for s in input:
+		numbers = [char for char in s if char.isdigit()]
+
+		if numbers:
+			first = numbers[0]
+			last = numbers[-1]
+
+			results.append(int(first + last)) 
+ 
+	return results
+	
+total = sum(extract_callibration_value(input))
+
+print(total)
+		
